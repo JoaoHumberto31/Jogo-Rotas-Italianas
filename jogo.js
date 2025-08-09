86ced305b3e4058c80afd084434c8e1e6905167f
Joao Humberto Tecila Mayer <joao_h_mayer@estudante.sesisenai.org.br>
2025-08-08 22:35:11 -0300

    dia 1 (parte 2)

--- jogo.js
+++ jogo.js
@@ -11,15 +11,28 @@ const jump = () => {
 
 const loop = setInterval(() => {
 
-    const pizzaPosition = pizza.offsetleft;
+    const pizzaPosition = +window.getComputedStyle(pizza).left.replace('px', '')
+    const pugPosition = +window.getComputedStyle(pug).bottom.replace('px', '');
 
-    if(pizzaPosition <= 118 && pug <= -40) {
+    if(pizzaPosition <= 118 && pizzaPosition > 0 && pugPosition < -8) {
 
         pizza.style.animation = 'none';
+        pizza.style.left = `${pizzaPosition}px`;
+
+        pug.style.animation = 'none';
+        pug.style.bottom = `${pugPosition}px`;
+        
+
+        pug.src = './img/pugViniTriste.png'
+        pug.style.width = '90px'
+        pug.style.marginLeft = '80px'
+        pug.style.marginBottom = '80px'
+
+        clearInterval(loop)
 
     }
 
-}, 10);
+}, 5);
 
 document.addEventListener('keydown', (event) => {
     if(event.code == 'Space'){
